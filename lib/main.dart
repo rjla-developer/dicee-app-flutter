e9570b32f379180038b0d079768ada6180b81006
RJ Luengas <robertojla8@gmail.com>
2023-12-11 22:19:25 -0600

    Se agregaron imagenes y un botón

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,26 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: TextButton(
+                  onPressed: () { },
+                  child: Image.asset('images/dice1.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Image.asset('images/dice2.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
